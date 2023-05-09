7697331e1659c4fd8fbf897de7107aa1f494c476
Loreli <dejesus.loreli@gmail.com>
2023-05-09 12:18:18 +1000

    first commit

---
+++ src/Pages/Inspections.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Inspections() {
+
+    return (
+        <div>
+            <h1> Here are your list of inspections </h1>
+            <ul>
+                <li>
+                    inspection 1
+                </li>
+                <li>
+                    inspection 2
+                </li>
+                <li>
+                    inspection 3
+                </li>
+            </ul>
+        </div>
+    );
+
+}
